e0978d5f5866083959879910af90b86cc3bc712f
jryano123 <johnreyano05@gmail.com>
2023-06-13 18:30:34 +0800

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        using (StreamReader reader = new StreamReader("C:\\Users\\Administrator\\Desktop\\VB.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        
+        using (StreamWriter writer = File.AppendText("C:\\Users\\Administrator\\Desktop\\VB.txt"))
+        {
+            string message = "This is my streamwriter logging details " + DateTime.Now.ToString() + ".";
+            writer.WriteLine(message);
+        }
+
+        Console.WriteLine("Logs written successfully.");
+        Console.ReadKey();
+    }
+}
